3f5db95527fae7e2e61ca3edb6aeac5c0f56ebf7
Jawand Singh <jawands@amdocs.com>
2019-12-27 12:19:45 +0530

    add movie component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import MovieComponent from "./components/MovieComponent";
 
 function App() {
   return (
     <main className="container">
       <h1>Hello World</h1>
+      <MovieComponent></MovieComponent>
     </main>
   );
 }
